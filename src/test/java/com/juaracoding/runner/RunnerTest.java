1db71da399690c9a3ca3ce61b14042c52a2b03a3
C-Moons <hendraa.ardiansyah@gmail.com>
2025-12-29 19:18:57 +0700

    update

--- src/test/java/com/juaracoding/runner/RunnerTest.java
+++ src/test/java/com/juaracoding/runner/RunnerTest.java
@@ -3,10 +3,13 @@ package com.juaracoding.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = { "src/test/resources/features/ImportAbsen.feature", "src/test/resources/features/ImportStatusAktif.feature", "src/test/resources/features/ImportStrukturUser.feature", "src/test/resources/features/ImportCuti.feature"}, glue = {
+@CucumberOptions(features = { "src/test/resources/features/ImportAbsen.feature",
+        "src/test/resources/features/ImportStatusAktif.feature",
+        "src/test/resources/features/ImportStrukturUser.feature",
+        "src/test/resources/features/ImportCuti.feature" }, glue = {
                 "com.juaracoding.definition",
                 "com.juaracoding.hook",
-}, plugin = { "pretty", "html:test-output", "json:target/cucumber/cucumber.json",
+        }, plugin = { "pretty", "html:test-output", "json:target/cucumber/cucumber.json",
                 "html:target/cucumber-html-report",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 public class RunnerTest extends AbstractTestNGCucumberTests {
